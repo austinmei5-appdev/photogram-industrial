e5b4aef7f4e64f14bd3a98c63fc7d9716fdd6f4c
austinmei5 <austinmei@uchicago.edu>
2023-01-14 21:02:17 +0000

    added direct associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,8 +28,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
-  has_many :comments, foreign_key: "author_id"
-  has_many :outgoing_requests, class_name: "Follow_Request", foreign_key: "sender_id"
-  has_many :incoming_requests, class_name: "Follow_Request", foreign_key: "recipient_id"
+  has_many :own_photos, foreign_key: :owner_id, class_name: "Photo"
+  has_many :comments, foreign_key: :author_id
+
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+
+  has_many :likes, foreign_key: :fan_id
+
+  # has_many :outgoing_requests, class_name: "Follow_Request", foreign_key: "sender_id"
+  # has_many :incoming_requests, class_name: "Follow_Request", foreign_key: "recipient_id"
 end
