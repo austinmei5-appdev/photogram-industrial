609a404a49f22296d8a4d8a5556bf529bccce4de
austinmei5 <austinmei@uchicago.edu>
2023-01-15 05:14:13 +0000

    added partial templates to photos

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,13 @@ Rails.application.routes.draw do
   resources :photos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/:username" => "users#show"
+
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "photos#liked", as: :liked_photos
+  get ":username/followers" => "photos#liked", as: :liked_photos
+  get ":username/following" => "photos#liked", as: :liked_photos
+
+  get ":username" => "users#show", as: :user
   
 
 
